36746a38f2c93cd485413a1b88f89903a1632474
Irina Chuprakova <mephistopheles961@gmail.com>
2022-09-01 22:51:33 +0300

    .

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,4 +1,6 @@
-const origins = 'http://mestoirina.nomoredomains.sbs/';
+const origins = [
+  'http://mestoirina.nomoredomains.sbs/',
+  'http://localhost:3000'];
 
 module.exports = (req, res, next) => {
   res.header('Access-Control-Allow-Headers', origins);
