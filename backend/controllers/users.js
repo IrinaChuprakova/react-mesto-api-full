348c2eefeb9e8220299737cb24ba36c012cfc2be
Irina Chuprakova <mephistopheles961@gmail.com>
2022-09-04 21:13:17 +0300

    test

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -13,8 +13,6 @@ const login = (req, res, next) => {
       res
         .cookie('jwt', token, {
           maxAge: 3600000 * 24 * 7,
-          httpOnly: true,
-          sameSite: 'none',
         })
         .status(200)
         .send({
