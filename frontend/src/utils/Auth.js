1d300227b771f77ffba530c62e79812cbfdb1221
Irina Chuprakova <mephistopheles961@gmail.com>
2022-09-13 12:20:27 +0300

    test

--- frontend/src/utils/Auth.js
+++ frontend/src/utils/Auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://api.MestoIrina.nomoredomains.sbs.nomoredomains.sbs';
+export const BASE_URL = 'http://MestoIrina.nomoredomains.sbs/api';
 
 function checkStatus(res){
   if (res.ok){
